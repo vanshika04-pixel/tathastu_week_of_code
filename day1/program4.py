5d52c8ded3b238f98063ded1bb94ba8c137116fe
vanshika04-pixel <65335750+vanshika04-pixel@users.noreply.github.com>
2020-05-20 11:56:34 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,6 @@
+cost_price=float(input("enter cost price"))
+sell_price=float(input("enter selling price"))
+profit=sell_price-cost_price
+new_sellprice=1.05*profit+cost_price
+print(profit)
+print(new_sellprice)
