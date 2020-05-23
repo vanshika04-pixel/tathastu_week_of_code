96d0cee830b365065ee7b79e73c7d19c618cdb4c
vanshika saxena <65335750+vanshika04-pixel@users.noreply.github.com>
2020-05-23 18:49:51 +0530

    Update program19.py

--- day 4/program19.py
+++ day 4/program19.py
@@ -19,11 +19,9 @@ student_data = {'id1':
     'subject_integration': ['english, math, science']
    },
 }
-
 result = {}
 
 for key,value in student_data.items():
     if value not in result.values():
         result[key] = value
-
-print(result
+print(result)
